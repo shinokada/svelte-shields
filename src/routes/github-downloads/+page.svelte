5d891e604c283d345516fc97ab2cb05d27d80b96
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-14 18:24:56 +0200

    docs: github-downloads update

--- src/routes/github-downloads/+page.svelte
+++ src/routes/github-downloads/+page.svelte
@@ -43,3 +43,55 @@
 <CodeWrapper>
 <GitHubDownloads {...download1} />
 </CodeWrapper>
+
+<H2>Examples</H2>
+
+<H3>Basic usage</H3>
+
+<CodeWrapper>
+  <GitHubDownloads {...download1} />
+</CodeWrapper>
+
+<HighlightCompo codeLang="ts" code={modules['./md/basic.md'] as string} />
+
+<H3>Style</H3>
+<p>Possible values: flat, flat-square, plastic, for-the-badge, social.
+  If not specified, the default style for this badge is "flat".</p>
+
+  <CodeWrapper c_w_div='grid gap-4'>
+    <GitHubDownloads {...download1} />
+    <GitHubDownloads {...download1} />
+    <GitHubDownloads {...download1} />
+    <GitHubDownloads {...download1} />
+    <GitHubDownloads {...download1} />
+</CodeWrapper>
+
+<HighlightCompo codeLang="ts" code={modules['./md/style.md'] as string} />
+
+
+<H2>Color</H2>
+<p>Background color of the right part (hex, rgb, rgba, hsl, hsla and css named colors supported).</p>
+
+<CodeWrapper c_w_div='grid gap-4'>
+  <GitHubDownloads {...download1} />
+</CodeWrapper>
+
+<H2>Logo & Label</H2>
+
+
+
+<H2>Link</H2>
+<p>Specify what clicking on the left/right of a badge should do.</p>
+<CodeWrapper>
+  <GitHubDownloads {...download1} />
+</CodeWrapper>
+
+<HighlightCompo codeLang="ts" code={modules['./md/link.md'] as string} />
+
+<H2>Other</H2>
+<p><Code>cacheSeconds</Code> is HTTP cache lifetime (rules are applied to infer a default value on a per-badge basis, any values specified below the default will be ignored).</p>
+<CodeWrapper>
+  <GitHubDownloads {...download1} />
+</CodeWrapper>
+
+<HighlightCompo codeLang="ts" code={modules['./md/other.md'] as string} />
