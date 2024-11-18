03b9657ec8f7205ca265f134101323cf23dc9953
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-18 12:53:18 +0100

    fix: static-badge page version data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,13 @@
 import type { MetaProps } from 'runes-meta-tags';
 import { ANALYTICS_ID_RUNES_LIB } from '$env/static/private';
+// import { fetchPackageVersions } from '$lib/utils/npm-version';
+// import { svelte5_icons } from '$lib/data/icons';
 
 const title = 'Svelte Shields';
 const description = 'A collection of Shilds badge components for Svelte Runes';
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-shields';
 const keywords = 'svelte, runes, sveltekit, shields.io, badges';
+// const versions = fetchPackageVersions(svelte5_icons);
 
 export const load = ({ url }) => {
   const layoutMetaTags: MetaProps = {
--- src/routes/guide/static-badge/examples/Dynamic.svelte
+++ src/routes/guide/static-badge/examples/Dynamic.svelte
@@ -3,26 +3,49 @@
   import { svelte5_icons } from '$lib/data/icons';
   import { StaticBadge } from '$lib';
   let { data } = $props<{ data: PageData }>();
+  interface VersionInfo {
+    latest: string;
+    previousStable: string;
+  }
+
+  interface Versions {
+    [key: string]: VersionInfo;
+  }
+  
+  let versions = $state<Versions>({});
+  let isLoading = $state(true);
+  
+  // Handle the Promise without making the effect async
+  $effect(() => {
+    data.versions.then((versionData: Versions) => {
+      versions = versionData;
+      isLoading = false;
+    });
+  });
   
   function safeReplace(packageName: string | undefined) {
     return typeof packageName === 'string' ? packageName.replace(/-/g, '--') : '';
   }
-  // console.log('data: ', data)
 </script>
 
+
 {#snippet runesIcon({ packageName }: LibType)}
-  <StaticBadge
-    badgeContent={safeReplace(packageName) +
-      '-' +
-      (data?.versions[packageName]?.latest || '') +
-      '-blue'}
-  />
-  <StaticBadge
-    badgeContent={safeReplace(packageName) +
-      '-' +
-      (data?.versions[packageName]?.previousStable || '') +
-      '-blue'}
-  />
+  {#if isLoading}
+    <StaticBadge badgeContent="loading..." />
+  {:else}
+    <StaticBadge
+      badgeContent={safeReplace(packageName) +
+        '-' +
+        (versions[packageName]?.latest || '') +
+        '-blue'}
+    />
+    <StaticBadge
+      badgeContent={safeReplace(packageName) +
+        '-' +
+        (versions[packageName]?.previousStable || '') +
+        '-blue'}
+    />
+  {/if}
 {/snippet}
 
 <div class="flex flex-wrap justify-center gap-6">
